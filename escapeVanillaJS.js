82f1d7345f2b16af6691a049140e6787b138fe3b
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-04-16 13:51:41 +0200

    fixed function call error added async

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -15,12 +15,12 @@ document.addEventListener("DOMContentLoaded", () => {
         // 🪲 Bug: What's mssing from JS concepts?
         const reactConcepts = new Set(['components', 'jsx', 'hooks', 'async']);
         // 🪲 Bug: Incorrect function call
-        const commonConcepts = findIntersection(jsConcepts, jsConcepts);
+        const commonConcepts = findIntersection(jsConcepts, reactConcepts);
         document.getElementById("room2Result").textContent = `The code to unlock the door is: ${Array.from(commonConcepts).join(', ')}`;
     });
 
     // 🪲 Bug: Asynchronous function ?
-    document.getElementById("solveRoom3").addEventListener("click", () => {
+    document.getElementById("solveRoom3").addEventListener("click", async () => {  //addedd async
         fetch('directions.json') 
             .then(response => response.json())
             .then(directions => {
